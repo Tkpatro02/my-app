e2909af075a83f31cc03fbfdbb26fb4f7baa1841
triyank.patro <triyank.patro@gmail.com>
2023-09-22 11:59:53 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
     <Navbar />
         <Routes>
-          <Route path="/home" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/courses" element={<AdminCourse />} />
           <Route path="/contact" element={<Addcourses />} />
         </Routes>
